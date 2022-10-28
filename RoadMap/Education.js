90ddf17593cea8c828c4a41c31645a569cedb0ba
IT_on_Minimal <moonstrike2018@gmail.com>
2022-10-28 21:47:49 +0300

    Educ Com

---
+++ RoadMap/Education.js
@@ -0,0 +1,12 @@
+document.querySelector("#Education").onclick = function () {
+    
+    let strs;
+alert( extractCurrencyValue('$120'), strs); // true
+
+
+function extractCurrencyValue(str) {
+  strs = +str.slice(1);
+  return strs;
+}
+}
+    
--- RoadMap/Objects.js
+++
@@ -1,7 +0,0 @@
-document.querySelector("#test2").onclick = function () {
-    function Sms(){
-    alert ("Вы нажали кнопку.");
-    }
-    Sms();
-}
-    
