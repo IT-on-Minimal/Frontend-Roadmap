33e18433acf7252699901d1c7e91129bcced6f50
IT_on_Minimal <moonstrike2018@gmail.com>
2022-10-28 19:16:10 +0300

    Srting 1

--- RoadMap/Objects.js
+++ RoadMap/Objects.js
@@ -1,14 +1,7 @@
 document.querySelector("#test2").onclick = function () {
-    let user = {
-        name: "John",
-        age: 30
-      };
-      
-function UserFunc(keys) {
- if(keys in user) {
-  console.log(user[keys])
- }
-}
-UserFunc('age')
+    function Sms(){
+    alert ("Вы нажали кнопку.");
+    }
+    Sms();
 }
     
--- RoadMap/String.js
+++ RoadMap/String.js
@@ -1,9 +1,32 @@
 document.querySelector("#String").onclick = function() {
 
-    function ucFirst(str) {
-    console.log(str[0].toUpperCase())
-    }
 
-    ucFirst('привет')
-       
+  function ucFirst(str) {
+    let numb1 = 1;
+    let numb2 = prompt('Введите число знаков', '');  
+     if(numb2 == '' || numb2 != Number(numb2)) {
+        alert('Вы ввели не число, попробуйте еще раз');
+        return ucFirst(str);
+     } else {
+        funks(numb1,numb2);
+     }
+     function funks(numb1,numb2){
+        console.log(str[0].toUpperCase());
+          let strs = str.slice(numb1,numb2);
+          for (let Upper of strs) {
+            console.log(Upper);
+            }      
+       }    
+        
+   }
+  function ifValue(){ 
+    let str = prompt('Введите любое слово', '');
+    if(str != Number(str)){
+    ucFirst(str);
+   }  else {
+     alert('Введите любое слово, пожалуйста');
+     return ifValue();
+     }
+   }
+   ifValue();
 }
